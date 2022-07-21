1809b47e217a208a03926865926a73816696f743
Hugo Santos <hf.santos2332@gmail.com>
2022-07-20 21:22:07 -0300

    Implementa leitura de arquivo de nível no argumento do snaze

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,15 @@ using namespace std;
 int main(int argc, char *argv[]){
 
     //TODO: processar os argumentos
-    SnakeGame game("../data/teste.txt");//* START
-    
-    game.loop();//* UPDATE
+    if(argc == 2){
+        string levelFile = argv[1];
+
+        SnakeGame game(levelFile);
+        
+        game.loop();
+    }else{
+        cout << "Argumentos inválidos para a execução do snaze! Lembre de informar o caminho do arquivo de níveis corretamente." << endl;
+    }
 
     return 0;
 }
